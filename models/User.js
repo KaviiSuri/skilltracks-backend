af06c8ffee4ba18cacf754ff89f44119ec59a879
KaviiSuri <surikavii@gmail.com>
2020-10-03 19:30:12 +0530

    fixed schema

--- models/User.js
+++ models/User.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
         },
         progress: [
           {
-            type: mongoose.mongo.Types.ObjectId,
+            type: mongoose.Types.ObjectId,
             ref: "Step",
           },
         ],
